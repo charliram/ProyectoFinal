b677249a74b6f5cb2c27ce241949009aaef29207
Ferosnola <ferchoaavila@gmail.com>
2021-06-08 17:30:52 -0500

    version 1.6
    hola

---
+++ ProyectoFinal/src/misclases/Huesped.java
@@ -0,0 +1,12 @@
+
+package misclases;
+import java.util.Calendar;
+
+public class Huesped {
+    String nombre;
+    int hab;
+    String ciudadOrigen;
+    Calendar ingreso;
+    int personas;
+    int estancia;
+}
